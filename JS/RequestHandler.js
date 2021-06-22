7966fa5eff76215deb3dd76fd9c37536305ddf0a
msdfsf <fsfdsm@gmail.com>
2021-06-23 01:11:23 +0200

    meh...

--- JS/RequestHandler.js
+++ JS/RequestHandler.js
@@ -16,6 +16,24 @@ function httpGet(url, callback, headers, sync = false) {
 
 }
 
+function httpPost(url, callback, headers, sync = false) {
+    
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+
+    xmlHttp.open('POST', url, !sync);
+    
+	for (i = 0; i < headers.length; i++) {
+        xmlHttp.setRequestHeader(headers[i].key, headers[i].value);
+    }
+	
+	xmlHttp.send(null);
+
+}
+
 function setCookie(name,value,days) {
     var expires = "";
     if (days) {
